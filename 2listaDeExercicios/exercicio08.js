3e8b40608583a9497ed18c94b1657d4a2a4738b8
Daniel Silva <daniel.silva@al.infnet.edu.br>
2026-09-03 10:45:24 -0300

    feat: implementa multiplicacao sem operador * (exercicio08)

---
+++ 2listaDeExercicios/exercicio08.js
@@ -0,0 +1,12 @@
+const multiplicar = (numero1, numero2) => {  
+    
+let resultado = 0
+    for(let i = 0;  i < numero2; i++) {
+        resultado += numero1
+    }
+    return resultado
+    
+} 
+
+console.log(multiplicar(5, 5))
+console.log(multiplicar(0, 7))
